5638c9ad59de73c3353e6b020942782d44a8897a
Bearnadetta <bearnadetta@gmail.com>
2021-08-19 08:54:37 -0700

    functional eventlistener for mouseover added

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const container = document.getElementById('container');
 
 const onClear = function (e) {
+    container.innerHTML = '';
     container.style.setProperty('--grid-rows', e);
     container.style.setProperty('--grid-cols', e);
     for (let i = 0; i < (e* e); i++){
         let square = document.createElement('div');
         square.className = 'square';
+        square.addEventListener('mouseenter', function(e){
+            e.target.style.backgroundColor = 'black';
+        });
         container.appendChild(square);
+
 
     };
 };
 
-onClear(16);
 
+
+onClear(16);
